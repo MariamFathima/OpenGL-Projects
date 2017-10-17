b2afaf28c6687461b1f4e4fc0e638ca114fea7d0
agune001 <adi.vtca@gmail.com>
2017-10-17 14:14:55 -0700

    applied worldtex texture

--- Texture-Mapping/sample.cpp
+++ Texture-Mapping/sample.cpp
@@ -234,11 +234,48 @@ float	Dot(float [3], float [3]);
 void	Cross(float [3], float [3], float [3]);
 float	Unit(float[3], float[3]);
 void	MjbSphere(float, int, int);
+unsigned char * BmpToTexture(char *, int *, int *);
+int		texWidth;
+int		texHeight;
+//variables for texture conversion:
+
+int		ReadInt(FILE *);
+short	ReadShort(FILE *);
+
+
+struct bmfh
+{
+	short bfType;
+	int bfSize;
+	short bfReserved1;
+	short bfReserved2;
+	int bfOffBits;
+} FileHeader;
+
+struct bmih
+{
+	int biSize;
+	int biWidth;
+	int biHeight;
+	short biPlanes;
+	short biBitCount;
+	int biCompression;
+	int biSizeImage;
+	int biXPelsPerMeter;
+	int biYPelsPerMeter;
+	int biClrUsed;
+	int biClrImportant;
+} InfoHeader;
+
+const int birgb = { 0 };
+
 // main program:
 
 int
 main( int argc, char *argv[ ] )
 {
+	texWidth = 1024;
+	texHeight = 512;
 	//seed our rand to make things interesting
 	srand(time(NULL));
 
@@ -431,7 +468,29 @@ Display( )
 			glCallList( BoxList );
 		glPopMatrix( );
 	}*/
+	unsigned char* Tex = BmpToTexture("worldtex.bmp", &texWidth, &texHeight);;
+	int level, ncomps, border;
+	level = 0;
+	ncomps = 3;
+	border = 0;
+
 	
+
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+
+	glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
+	glTexImage2D(GL_TEXTURE_2D, level, ncomps, texWidth, texHeight, border, GL_RGB, GL_UNSIGNED_BYTE, Tex);
+
+	glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_REPLACE);
+		
+	glMatrixMode(GL_TEXTURE);
+	glTranslatef(0., 0., 0.);
+	glEnable(GL_TEXTURE_2D);
+
 	MjbSphere(RADIUS, SLICES, STACKS);
 	
 	// draw some gratuitous text that just rotates on top of the scene:
@@ -1355,4 +1414,138 @@ MjbSphere(float radius, int slices, int stacks)
 
 	delete[] Pts;
 	Pts = NULL;
+}
+
+/* BMPTOTEXTURE.CPP STARTS HERE */
+unsigned char *
+BmpToTexture(char *filename, int *width, int *height)
+{
+
+	int s, t, e;		// counters
+	int numextra;		// # extra bytes each line in the file is padded with
+	FILE *fp;
+	unsigned char *texture;
+	int nums, numt;
+	unsigned char *tp;
+
+
+	fp = fopen(filename, "rb");
+	if (fp == NULL)
+	{
+		fprintf(stderr, "Cannot open Bmp file '%s'\n", filename);
+		return NULL;
+	}
+
+	FileHeader.bfType = ReadShort(fp);
+
+
+	// if bfType is not 0x4d42, the file is not a bmp:
+
+	if (FileHeader.bfType != 0x4d42)
+	{
+		fprintf(stderr, "Wrong type of file: 0x%0x\n", FileHeader.bfType);
+		fclose(fp);
+		return NULL;
+	}
+
+
+	FileHeader.bfSize = ReadInt(fp);
+	FileHeader.bfReserved1 = ReadShort(fp);
+	FileHeader.bfReserved2 = ReadShort(fp);
+	FileHeader.bfOffBits = ReadInt(fp);
+
+
+	InfoHeader.biSize = ReadInt(fp);
+	InfoHeader.biWidth = ReadInt(fp);
+	InfoHeader.biHeight = ReadInt(fp);
+
+	nums = InfoHeader.biWidth;
+	numt = InfoHeader.biHeight;
+
+	InfoHeader.biPlanes = ReadShort(fp);
+	InfoHeader.biBitCount = ReadShort(fp);
+	InfoHeader.biCompression = ReadInt(fp);
+	InfoHeader.biSizeImage = ReadInt(fp);
+	InfoHeader.biXPelsPerMeter = ReadInt(fp);
+	InfoHeader.biYPelsPerMeter = ReadInt(fp);
+	InfoHeader.biClrUsed = ReadInt(fp);
+	InfoHeader.biClrImportant = ReadInt(fp);
+
+
+	// fprintf( stderr, "Image size found: %d x %d\n", ImageWidth, ImageHeight );
+
+
+	texture = new unsigned char[3 * nums * numt];
+	if (texture == NULL)
+	{
+		fprintf(stderr, "Cannot allocate the texture array!\b");
+		return NULL;
+	}
+
+
+	// extra padding bytes:
+
+	numextra = 4 * (((3 * InfoHeader.biWidth) + 3) / 4) - 3 * InfoHeader.biWidth;
+
+
+	// we do not support compression:
+
+	if (InfoHeader.biCompression != birgb)
+	{
+		fprintf(stderr, "Wrong type of image compression: %d\n", InfoHeader.biCompression);
+		fclose(fp);
+		return NULL;
+	}
+
+
+
+	rewind(fp);
+	fseek(fp, 14 + 40, SEEK_SET);
+
+	if (InfoHeader.biBitCount == 24)
+	{
+		for (t = 0, tp = texture; t < numt; t++)
+		{
+			for (s = 0; s < nums; s++, tp += 3)
+			{
+				*(tp + 2) = fgetc(fp);		// b
+				*(tp + 1) = fgetc(fp);		// g
+				*(tp + 0) = fgetc(fp);		// r
+			}
+
+			for (e = 0; e < numextra; e++)
+			{
+				fgetc(fp);
+			}
+		}
+	}
+
+	fclose(fp);
+
+	*width = nums;
+	*height = numt;
+	return texture;
+}
+
+
+
+int
+ReadInt(FILE *fp)
+{
+	unsigned char b3, b2, b1, b0;
+	b0 = fgetc(fp);
+	b1 = fgetc(fp);
+	b2 = fgetc(fp);
+	b3 = fgetc(fp);
+	return (b3 << 24) | (b2 << 16) | (b1 << 8) | b0;
+}
+
+
+short
+ReadShort(FILE *fp)
+{
+	unsigned char b1, b0;
+	b0 = fgetc(fp);
+	b1 = fgetc(fp);
+	return (b1 << 8) | b0;
 }
